b8bac56b0b2053f90ccb6bd2c99a8fd136ba4f06
tanedward2101 <tanedward2101@gmail.com>
2022-01-17 21:32:57 +0700

    deadlarvaz

--- pages/larva/[id].js
+++ pages/larva/[id].js
@@ -44,7 +44,7 @@ export default function Larva() {
   )
 
   if (error) return <div>{error.message}</div>
-  if (!data) return <div></div>
+  if (!data) return JSON.stringify(data)
   if (query.id > supply) {
     return (<div></div>)
   }

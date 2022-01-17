0f4ad68a8b99fa91974a238696dc4b47f986bcf7
tanedward2101 <tanedward2101@gmail.com>
2022-01-17 23:38:08 +0700

    try

--- pages/api/dead/[id].js
+++ pages/api/dead/[id].js
@@ -7,11 +7,27 @@ import { useEffect, useState } from 'react';
 export default function personHandler({ query: { id } }, res) {
 
   // User with id exists
+  const provider = `https://rinkeby.infura.io/v3/10475d80c3ba4b3d8f768b09dd3faafc`;
+  var amount=0;
+  const web3 = new Web3(
+    new Web3.providers.HttpProvider(provider),
+  );
+getSupply();
 
-  const filtered = deadlarva.filter((p) => p.id === id)
-  if (filtered.length > 0 && id ) {
-    res.status(200).json(filtered[0])
-  } else {
-    res.status(404).json({ message: `Larva with id: ${id} not found.` })
+  async function getSupply() {
+    const contract = new web3.eth.Contract(
+      CC_ABI,
+      '0xA7f5BEd28c596afEc2980C29874963A974461Cf1',
+    )
+    amount = await contract.methods.totalSupply().call()
+    const filtered = deadlarva.filter((p) => p.id === id)
+    if (filtered.length > 0 && id <amount) {
+      res.status(200).json(filtered[0])
+    } else {
+      res.status(404).json({ message: `Larva with id: ${id} not found.` })
+    }
   }
+
+
+ 
 }
